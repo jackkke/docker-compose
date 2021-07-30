c77842176bb9eb570f18871b35aac3b13cfa4b9a
韩高杰 <hangj@asiainfo.com>
2021-07-30 11:04:03 +0800

    k8s

--- k8s/2/load_images.sh
+++ k8s/2/load_images.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-file="images"
+file="image_list"
 
 if [ -f "$file" ]
 then
-  echo "$file found."
+  echo "file $file found."
 
   while IFS='=' read -r key value
   do
     #echo "${key}=${value}"
     docker pull ${value}
     docker tag ${value} ${key}
+    echo "docker pull ${key} success."
     docker rmi ${value}
   done < "$file"
 
 else
-  echo "$file not found."
+  echo "file $file not found."
 fi
